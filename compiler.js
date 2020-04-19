d396ccc225661d7031dc77138e84aefa9770094b
RowenStipe <rowen.stipe@gmail.com>
2020-04-18 23:47:58 -0400

    added compiler functionality

--- compiler.js
+++ compiler.js
@@ -1,16 +1,18 @@
+const fs = require('fs');
+const archiver = require('archiver');
 const compileTime = new Date();
 
 const workingDir = `./Grim And Sons`;
-const fs = require('fs');
-const debug = false;
+const debug = true;
 
 const config = JSON.parse(fs.readFileSync(`${workingDir}/lcp_manifest.json`));
 const manufacturers = JSON.parse(fs.readFileSync(`${workingDir}/manufacturers.json`));
 
-const compileDir = `${workingDir}/${config.name}-v${config.version}-${compileTime.getHours()}${compileTime.getMinutes()}${compileTime.getSeconds()}-${compileTime.getDate()}_${compileTime.getMonth()}_${compileTime.getFullYear()}`;
+let compileDir = `${workingDir}/${config.name}-v${config.version}-${compileTime.getHours()}${compileTime.getMinutes()}${compileTime.getSeconds()}-${compileTime.getDate()}_${compileTime.getMonth()}_${compileTime.getFullYear()}`;
+let compileDirOptional = `${compileDir}-optional`
 
 function dbug(message){
-    if(debug){console.log(`[DEBUG] ${message}`)};
+    if(debug){console.log(`[DEBUG] ${message}`)}
 }
 
 function makeDir(folderName){
@@ -23,20 +25,19 @@ function makeDir(folderName){
 function makeJSONFile(type, DATA){
     fs.appendFileSync(`${type}.json`, JSON.stringify(DATA, null, '\t'), 'utf8', function (err) {
         if (err) {
-            console.error(`Error while writing JSON Object to File.`);
-            return console.log(err);
+            return console.error(`Error while writing JSON Object to File.`);
         }
         dbug("JSON file has been saved."); 
     });
 }
 
-function jsonSort(type){    
+function jsonSort(type, standard=true, optional=false){    
     let files = [];
     let DATA = [];
-    if(type === 'frames'){
+    if(type === 'frames' && standard){
         const dir = `${workingDir}/Frames/`;
         fs.readdirSync(dir).forEach(file => {files.push(file);});
-        dbug(`Initalizing Type: ${type}`)
+        dbug(`Initalizing Type: ${type}`);
         for (i = 0; i < files.length; i++) {
             console.log(`ITEM: ${dir}${files[i]}`);
             try {
@@ -44,57 +45,69 @@ function jsonSort(type){
                 dbug(`Outputting ${dir}${files[i]}`)
                 dbug(DATA);
             } catch (err) {console.error(err);}
-        };
-    } else if(type === 'weapons'){
+        }
+    } else if(type === 'weapons' && standard){
         const dir = `${workingDir}/Weapons/`;
         fs.readdirSync(dir).forEach(file => {files.push(file);});
-        dbug(`Initalizing Type: ${type}`)
+        dbug(`Initalizing Type: ${type}`);
         for (i = 0; i < files.length; i++) {
             console.log(`ITEM: ${dir}${files[i]}`);
             try {
                 DATA.push(JSON.parse(fs.readFileSync(`${dir}${files[i]}`)));
-                dbug(`Outputting ${dir}${files[i]}`)
+                dbug(`Outputting ${dir}${files[i]}`);
                 dbug(DATA);
             } catch (err) {console.error(err);}
-        };
-    } else if(type === 'tags'){
+        }
+    } else if(type === 'tags' && standard){
         const dir = `${workingDir}/Tags/`;
         fs.readdirSync(dir).forEach(file => {files.push(file);});
-        dbug(`Initalizing Type: ${type}`)
+        dbug(`Initalizing Type: ${type}`);
         for (i = 0; i < files.length; i++) {
             console.log(`ITEM: ${dir}${files[i]}`);
             try {
                 DATA.push(JSON.parse(fs.readFileSync(`${dir}${files[i]}`)));
-                dbug(`Outputting ${dir}${files[i]}`)
+                dbug(`Outputting ${dir}${files[i]}`);
                 dbug(DATA);
             } catch (err) {console.error(err);}
-        };
-    } else if(type === 'mods'){
+        }
+    } else if(type === 'mods' && standard){
         const dir = `${workingDir}/Mods/`;
         fs.readdirSync(dir).forEach(file => {files.push(file);});
-        dbug(`Initalizing Type: ${type}`)
+        dbug(`Initalizing Type: ${type}`);
         for (i = 0; i < files.length; i++) {
             console.log(`ITEM: ${dir}${files[i]}`);
             try {
                 DATA.push(JSON.parse(fs.readFileSync(`${dir}${files[i]}`)));
-                dbug(`Outputting ${dir}${files[i]}`)
+                dbug(`Outputting ${dir}${files[i]}`);
                 dbug(DATA);
             } catch (err) {console.error(err);}
-        };
-    } else if(type === 'systems'){
+        }
+    } else if(type === 'systems' && standard){
         const dir = `${workingDir}/Systems/`;
         fs.readdirSync(dir).forEach(file => {files.push(file);});
-        dbug(`Initalizing Type: ${type}`)
+        dbug(`Initalizing Type: ${type}`);
         for (i = 0; i < files.length; i++) {
             console.log(`ITEM: ${dir}${files[i]}`);
             try {
                 DATA.push(JSON.parse(fs.readFileSync(`${dir}${files[i]}`)));
-                dbug(`Outputting ${dir}${files[i]}`)
+                dbug(`Outputting ${dir}${files[i]}`);
                 dbug(DATA);
             } catch (err) {console.error(err);}
-        };
-    } else if(type === 'core_bonus'){
+        }
+    } else if(type === 'core_bonus' && standard){
         const dir = `${workingDir}/Core Bonuses/`;
+        fs.readdirSync(dir).forEach(file => {files.push(file);});
+        dbug(`Initalizing Type: ${type}`);
+        for (i = 0; i < files.length; i++) {
+            console.log(`ITEM: ${dir}${files[i]}`);
+            try {
+                DATA.push(JSON.parse(fs.readFileSync(`${dir}${files[i]}`)));
+                dbug(`Outputting ${dir}${files[i]}`);
+                dbug(DATA);
+            } catch (err) {console.error(err);}
+        }
+    } else if(type === 'pilot_gear' && standard){
+        const dir = `${workingDir}/Pilot Gear/`;
         fs.readdirSync(dir).forEach(file => {files.push(file);});
         dbug(`Initalizing Type: ${type}`)
         for (i = 0; i < files.length; i++) {
@@ -104,21 +117,88 @@ function jsonSort(type){
                 dbug(`Outputting ${dir}${files[i]}`)
                 dbug(DATA);
             } catch (err) {console.error(err);}
-        };
-    } else {console.error(`Type: ${type} not valid`);}
-    makeJSONFile(`${compileDir}/${type}`, DATA);
+        }
+    } else {console.error(`Type: ${type} not valid`); return null}
+    if(!optional){makeJSONFile(`${compileDir}/${type}`, DATA);}
+    else{makeJSONFile(`${compileDirOptional}/${type}`, DATA);}
 }
 
-function compile(){
-    makeDir(compileDir);
-    jsonSort('frames');
-    jsonSort('weapons');
-    jsonSort('tags');
-    jsonSort('mods');
-    jsonSort('systems');
-    jsonSort('core_bonus');
-    makeJSONFile(`${compileDir}/lcp_manifest`, config);
-    makeJSONFile(`${compileDir}/manufacturers`, manufacturers);
+function makeZip(optional=false){
+    let files = [];
+    let dir = compileDir;
+    let name = `${config.name}-v${config.version}-${compileTime.getHours()}${compileTime.getMinutes()}${compileTime.getSeconds()}-${compileTime.getDate()}_${compileTime.getMonth()}_${compileTime.getFullYear()}`;
+    if(optional){
+        name = `${name}-optional`;
+        dir = compileDirOptional
+    }
+    fs.readdirSync(dir).forEach(file => {files.push(file);});
+
+    var output = fs.createWriteStream(`${dir}/${name}.lcp`);
+    var archive = archiver('zip', {
+    zlib: { level: 5 } // Sets the compression level.
+    });
+
+    // listen for all archive data to be written
+    // 'close' event is fired only when a file descriptor is involved
+    output.on('close', function() {
+        console.log(archive.pointer() + ' total bytes');
+        console.log('archiver has been finalized and the output file descriptor has closed.');
+    });
+    
+    // This event is fired when the data source is drained no matter what was the data source.
+    // It is not part of this library but rather from the NodeJS Stream API.
+    // @see: https://nodejs.org/api/stream.html#stream_event_end
+    output.on('end', function() {
+        console.log('Data has been drained');
+    });
+    
+    // good practice to catch warnings (ie stat failures and other non-blocking errors)
+    archive.on('warning', function(err) {
+        if (err.code === 'ENOENT') {
+        console.error(err);
+        } else {
+        // throw error
+        throw err;
+        }
+    });
+    
+    // good practice to catch this error explicitly
+    archive.on('error', function(err) {
+        throw err;
+    });
+   
+    // pipe archive data to the file
+    archive.pipe(output);
+    
+    dbug(`Initalizing files to zip`)
+    for (i = 0; i < files.length; i++) {
+        console.log(`ZIPPING ITEM: ${dir}/${files[i]}`);
+        archive.append(fs.createReadStream(`${dir}/${files[i]}`), { name: `${files[i]}` });
+    };
+    archive.finalize();
 }
 
+function compile(optional=false){
+    
+    if(!optional){
+        makeDir(compileDir);
+        jsonSort('frames');
+        jsonSort('weapons');
+        jsonSort('tags');
+        jsonSort('mods');
+        jsonSort('systems');
+        jsonSort('core_bonus');
+        makeJSONFile(`${compileDir}/lcp_manifest`, config);
+        makeJSONFile(`${compileDir}/manufacturers`, manufacturers);
+    }else{
+        makeDir(compileDirOptional);
+        jsonSort('pilot_gear',true,true);
+        config.name = `${config.name} - Pilot Gear`;
+        makeJSONFile(`${compileDirOptional}/lcp_manifest`, config);
+    }
+}
 compile();
+makeZip();
+
+compile(true);
+makeZip(true);
